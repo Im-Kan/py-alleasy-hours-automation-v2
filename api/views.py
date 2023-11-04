70a9f7a7999cdab1da71b2dea9affebf4b92c34d
Campera <90940106+Im-Kan@users.noreply.github.com>
2023-11-03 22:20:44 -0300

    arrumando espaçamento das funções (minha lsp ficou enchendo o saco)

--- api/views.py
+++ api/views.py
@@ -5,13 +5,16 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
+
 @api_view(['GET'])
 def simple_response(request):
     return Response({"message": "OK"}, status=status.HTTP_200_OK)
 
+
 @api_view(['POST'])
 def convert(request):
     return Response(":)", status=status.HTTP_200_OK)
+
 
 def home(request):
     return render(request, 'home.html')
